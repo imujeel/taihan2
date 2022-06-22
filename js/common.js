17013d9d65f17add3e6c787d0939b6605a097f11
boringbillion <marpungsun@gmail.com>
2022-06-22 15:59:55 +0900

    first upload

---
+++ js/common.js
@@ -0,0 +1,73 @@
+let header = $('header'),
+    menu = header.find('.header_wrapper>nav>ul>li'),
+    headerHeight = header.outerHeight(),
+    newHeight = 0,
+    subMenu = menu.find('ul'),
+    link = menu.find('a');
+
+        //subMenu 높이 구하기
+    subMenu.each(function(){
+        if($(this).outerHeight()>newHeight){
+            newHeight = headerHeight + $(this).outerHeight();
+        }
+    });
+    header.hover(
+    function(){		
+        header.stop().animate({height:newHeight});
+    },
+    function(){
+        header.stop().animate({height:headerHeight});
+    }
+    );//DropDown menu
+
+// let navMenu = $('header nav > ul > li'),
+//     highLight = $('.menu_highlight');
+
+//     navMenu.mouseover(function(e){
+//     e.preventDefault();
+//     let newLeft = $(this).position().left;
+//     let newWidth = $(this).innerWidth();
+
+//     highLight.animate({left:newLeft, width:newWidth},250);
+    
+//     }); //hight nav
+
+let searchIcon = $('header .top_links .search'),
+    searchField = $('body .search_field'),
+    searchFieldExit = searchField.find('.search_exit');
+
+    searchIcon.click(function(e){
+    e.preventDefault();
+    searchField.addClass('active');
+    });
+    searchFieldExit.click(function(e){
+    e.preventDefault();
+    searchField.removeClass('active');
+    });//Search field
+
+
+    let showListBtn = $('.sub_menu_area button');
+        subMenList = $('.sub_menu_list');
+
+        subMenList.hide();
+        showListBtn.click(function(e){
+            $(this).next('.sub_menu_list').slideToggle();
+            $(this).find('span').text(function(i, text){
+                return text === "arrow_drop_down" ? "arrow_drop_up" : "arrow_drop_down";
+            })
+    });//dropDown subpage menu
+
+    let subPageMenu = $('.banner .sub_menu'),
+        subPageMenuOst = subPageMenu.offset().top;
+
+        $(window).scroll(function(){
+            let sct = $(this).scrollTop();
+            if(sct > subPageMenuOst){
+                subPageMenu.addClass('sticky');
+                header.css({display:'none'});
+
+            }else{
+                subPageMenu.removeClass('sticky');
+                header.css({display:'block'});
+            }
+    })//fixed submenu
---
+++ js/main.js
@@ -0,0 +1,119 @@
+	let g_interval;
+	$('#fullpage').fullpage({
+		//options here
+		autoScrolling:true,
+		scrollHorizontally: true,
+		slidesNavigation:true,
+		anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage','5thPage'],
+		menu: '#menu',
+		// fixedElements:'#header',
+		controlArrowsHTML:['<div class="material-symbols-rounded controls prev">arrow_back_ios</div>',
+		'<div class="material-symbols-rounded controls next">arrow_forward_ios</div>'],
+		afterLoad: function (origin, destination, direction) {
+			clearInterval(g_interval);
+			const lapse = 6000;
+			
+			if(destination.item.querySelectorAll('.fp-slides').length){
+			  g_interval = setInterval(function () {
+				fullpage_api.moveSlideRight();
+			  }, lapse);
+			}
+		  
+			//fixed header
+			if(destination.index != 0){
+				$('header').addClass('active');
+			}else{
+				$('header').removeClass('active'); 
+			}
+
+			if(destination.index = 1){
+				numAnimation();
+			};
+		}
+	});//fullPage
+
+	$('.fp-slidesNav').append(
+		'<div class="play_controls_btn">'+
+			'<img src="images/play.svg" alt="play button" class="play_btn play_controls_btn">' +
+			'<img src="images/pause.svg" alt="pause button" class="pause_btn play_controls_btn">'+
+			'</div>'
+		);
+	
+	let controlsBtn =$('.fp-slidesNav .play_controls_btn'),
+		pauseBtn = $('.pause_btn'),
+		playBtn = $('.play_btn');			
+		pauseBtn.addClass('active');
+
+		controlsBtn.click(function(){
+			$(this).toggleClass('active');
+			if($(this).hasClass('active')){
+				clearInterval(g_interval);
+			}else{
+				setInterval(function(){
+					$.fn.fullpage.moveSlideRight();
+				}, 6000);
+				
+			}
+		}); // mainslide play btn 
+
+	
+	let showHideToggleBtn = $('.show_hide_btn'),
+		showBtn = showHideToggleBtn.find('.show'),
+		hideBtn = showHideToggleBtn.find('.hide'),
+		newsContainer = $('.my_mainslide .small_wrapper'),
+		mainslidePager = $('.fp-slidesNav');
+
+		hideBtn.addClass('active');
+		showHideToggleBtn.click(function(){
+			if(hideBtn.hasClass('active') == true){
+				newsContainer.stop().animate({
+					bottom:'-180px',
+				},400);
+				mainslidePager.stop().animate({
+					bottom:'60px'
+				},400);
+				hideBtn.removeClass('active');
+				showBtn.addClass('active');
+			}else{
+				newsContainer.stop().animate({
+					bottom:'0px',
+				},400);
+				mainslidePager.stop().animate({
+					bottom:'240px'
+				},400);
+				hideBtn.addClass('active');
+				showBtn.removeClass('active');
+			}
+		});//smallNews toggle btn
+		
+function numAnimation(){
+	let comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',');
+		$('#years').animateNumber({number: 81,numberStep:comma_separator_number_step},{easing: 'swing',duration: 1700});
+		$('#group').animateNumber({number: 843,numberStep:comma_separator_number_step},{easing: 'swing',duration: 1700});
+		$('#public').animateNumber({number: 100,numberStep:comma_separator_number_step},{easing: 'swing',duration: 1700});
+		$('#sales').animateNumber({number: 1596842, numberStep:comma_separator_number_step},{easing: 'swing',duration: 1000});
+}// Number animation
+
+		let miniPager = $('.pagers_small a'),
+			currentIdx = 0,
+			slides = $('.whats_new .content_wrapper ul li');
+			
+		miniPager.click(function(e){
+			e.preventDefault();
+			miniPager.removeClass('active');
+			let $this = $(this);
+			$this.addClass('active');
+
+			let targetIdx = $this.index();
+			moveSlide(targetIdx);
+		});
+		function moveSlide(i){
+			let currentSlide = slides.eq(currentIdx);
+			let nextSlide = slides.eq(i);
+	
+			currentSlide.css({left:0}).stop().animate({left:'-100%'});
+			nextSlide.css({left:'100%'}).stop().animate({left:0});
+			currentIdx = i;
+		}// what's new mini slide
+		
+	
