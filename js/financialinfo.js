60c415208bee4c2a6b354ec6043b9f0f3d0e4b54
imujeel <marpungsun@gmail.com>
2022-07-06 11:33:50 +0900

    cookie 수정, about 이미지 경로 변경

--- js/financialinfo.js
+++ js/financialinfo.js
@@ -1,47 +1,55 @@
 const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
+    '1분기',
+    '2분기',
+    '3분기',
+    '4분기',
   ];
-  let data2021 = {
-      label: '2021',
+  let data2019 = {
+      label: '2019',
       backgroundColor: 'rgb(255, 99, 132)',
       borderColor: 'rgb(255, 0, 0)',
-      data: [10, 5, 5, 4, 10, 5, 20],
+      data: [3139, 3236, 3303, 4271],
   };
 
-  let data2022 = {
-      label: '2022',
-      backgroundColor: 'rgb(0, 0, 100)',
+  let data2020 = {
+      label: '2020',
+      backgroundColor: 'rgb(0, 0, 900)',
       borderColor: 'rgb(0, 0, 255)',
-      data: [0, 10, 5, 2, 20, 30, 45],
-    };   
+      data: [3630, 3783, 2798, 4272],
+    };
+    let data2021 = {
+        label: '2021',
+        backgroundColor: 'rgb(0, 0, 100)',
+        borderColor: 'rgb(0, 0, 255)',
+        data: [3917, 4142, 4563, 5989],
+        }; 
+    let data2022 = {
+        label: '2022',
+        backgroundColor: 'rgb(0, 0, 230)',
+        borderColor: 'rgb(0, 0, 255)',
+        data: [5138],
+        }; 
 
   const data = {
     labels: labels,
-    datasets: [data2021,data2022]
+    datasets: [data2019,data2020,data2021,data2022]
   };
 
   const config = {
     type: 'bar',
     data: data,
     options: {
-      maintainAspectRatio:false,
-      scales: {
-        x: {
-          stacked: true
-        },
-        y: {
-          stacked: true
-        }        
-      }
+      maintainAspectRatio:false
     }
   };
 
 const myChart = new Chart(
     document.getElementById('bar-chart'),
     config
-  );
+  );// bar chart.js
+
+  $( ".tab-wrapper" ).tabs({
+    hide: { duration: 100 },
+      show: { duration: 100 }
+  });
+  
